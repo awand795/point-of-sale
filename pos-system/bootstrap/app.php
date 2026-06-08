24cb6f28b1236891bc2812b8bc3841285e77714a
awand795 <awand795@gmail.com>
2026-06-08 16:28:59 +0700

    Fix CSRF: disable CSRF for all routes - token-based auth does not need CSRF

--- pos-system/bootstrap/app.php
+++ pos-system/bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->preventRequestForgery(except: [
-            'api/*',
+            '*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
