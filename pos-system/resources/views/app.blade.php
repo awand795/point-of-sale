86a0e0055682d593e25d39a417344c6e289704b3
awand795 <awand795@gmail.com>
2026-06-09 10:02:44 +0700

    Fix ViteManifestNotFoundException: skip @vite() when manifest doesn't exist on Vercel

--- pos-system/resources/views/app.blade.php
+++ pos-system/resources/views/app.blade.php
@@ -39,8 +39,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @if (file_exists(public_path('build/manifest.json')))
+            @viteReactRefresh
+            @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @endif
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
