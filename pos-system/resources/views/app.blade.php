326e64f55b16ded9a270320afe37129b4404c483
awand795 <awand795@gmail.com>
2026-06-09 10:03:05 +0700

    Fix: also check for local environment so @vite() works with dev server

--- pos-system/resources/views/app.blade.php
+++ pos-system/resources/views/app.blade.php
@@ -39,7 +39,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @if (file_exists(public_path('build/manifest.json')))
+        @if (app()->environment('local') || file_exists(public_path('build/manifest.json')))
             @viteReactRefresh
             @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @endif
